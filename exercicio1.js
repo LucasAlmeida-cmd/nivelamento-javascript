50595a70845914719f15527b9666488ff097b836
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-02-14 10:49:09 -0300

    Conclusão do primeiro exercicio.

---
+++ exercicio1.js
@@ -0,0 +1,18 @@
+nome = "Lucas";
+idade = 22;
+aprovado = true;
+notas = [10, 10, 10];
+usuario = {
+    email: "lucas@gmail.com",
+    senha: "123456"
+};
+console.log(
+    `Nome: ${nome}
+Idade: ${idade}
+Aprovado: ${aprovado}
+Notas: ${notas}
+Usuario: ${usuario.email}
+E a diferença entre null e undefined é que null a variável foi 
+declarada mas não tem nenhum valor atribuído a ela, enquanto undefined é 
+quando a variável foi declarada mas não foi atribuído nenhum valor a ela.`
+)
